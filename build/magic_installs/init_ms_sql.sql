b99211dc23990c7b719dc5c89c19b1ce7d8f6acc
goodsy <paul.thomas.goodall@gmail.com>
2022-06-23 17:05:18 +1000

    Update init_ms_sql.sql

--- build/magic_installs/init_ms_sql.sql
+++ build/magic_installs/init_ms_sql.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE myapp;
+CREATE DATABASE my_db;
 go
 
-USE myapp;
+USE my_db;
 go
 
-CREATE LOGIN myapp_login WITH PASSWORD='My_App_1234', DEFAULT_DATABASE=myapp;
+CREATE LOGIN my_db_login WITH PASSWORD='My_App_1234', DEFAULT_DATABASE=my_db;
 go
 
-CREATE USER myapp FOR LOGIN myapp_login WITH DEFAULT_SCHEMA=dbo;
+CREATE USER my_db_user FOR LOGIN my_db_login WITH DEFAULT_SCHEMA=dbo;
 go
 
-ALTER ROLE db_owner ADD MEMBER myapp;
+ALTER ROLE db_owner ADD MEMBER my_db_user;
 go
